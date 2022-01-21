b83345211a0ba2b5c96b14dc6280f8e6ef8d0871
Anastasia Perez <Stasiperez85@gmail.com>
2022-01-20 19:36:15 -0700

    add saveBtn code

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,6 +1,17 @@
-// Display current day at top of calendar
+// Display current date at top of calendar
 var rightNow = moment().format("MMMM Do, YYYY");
 $("#currentDay").append(rightNow);
 console.log(rightNow);
+
+$(document).ready(function (){
+    $(".saveBtn").on("click", function(event){
+       
+        console.log(this);
+        var =
+        var =
+    })
+
+    localStorage.setItem(,)
+});
 
 // color-code time blocks to indicate past, present, or future
